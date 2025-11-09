d2d94c128104c453af0b455c729199d68367d326
Nabil <nabil.sebtaoui@esprit.tn>
2025-11-09 20:02:20 +0100

    feat: Initial commit - Architecture microservices LogosTech avec Urgences, Config Server, Eureka Server et API Gateway

---
+++ demoApiGateway/src/main/java/com/ms/apigateway/demoapigateway/DemoApiGatewayApplication.java
@@ -0,0 +1,31 @@
+package com.ms.apigateway.demoapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DemoApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiGatewayApplication.class, args);
+	}
+
+
+	@Bean
+	public RouteLocator  gatewayRoutes(RouteLocatorBuilder builder)
+	{
+		return builder.routes()
+				.route("routecandidat",r->r.path("/candidats/**")
+						.uri("lb://demoCandidat5SE2"))
+				.route("routejob",r->r.path("/jobs/**")
+						.uri("lb://MS-job-s"))
+				.build();
+
+
+	}
+}
